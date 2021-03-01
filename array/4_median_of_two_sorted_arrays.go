ef77e386de3ae2381d36a7c2c4cc41e586e8b30c
libaoan <baoan_008@163.com>
2021-03-02 07:01:36 +0800

    second method for median_of_two_sorted_arrays.go

--- array/4_median_of_two_sorted_arrays.go
+++ array/4_median_of_two_sorted_arrays.go
@@ -6,6 +6,10 @@ func main(){
 	fmt.Println(findMedianSortedArrays1([]int{}, []int{}))
 	fmt.Println(findMedianSortedArrays1([]int{1,3,5}, []int{2,4,6}))
 	fmt.Println(findMedianSortedArrays1([]int{1,3,5,7}, []int{2,3,6}))
+	fmt.Println("******************")
+	fmt.Println(findMedianSortedArrays2([]int{}, []int{}))
+	fmt.Println(findMedianSortedArrays2([]int{1,3,5}, []int{2,4,6}))
+	fmt.Println(findMedianSortedArrays2([]int{1,3,5,7}, []int{2,3,6}))
 
 }
 
@@ -56,3 +60,49 @@ func merge(nums1, nums2 []int) []int{
 // 类二分查找分而治之的思路
 // O(log(M+N)) // ok
 
+func findMedianSortedArrays2(nums1 []int, nums2 []int) float64{
+	n1, n2 := len(nums1), len(nums2)
+	if n1 + n2 == 0 {
+		return -1
+	}
+	if (n1+n2)%2 ==0 {
+		l := findKth(nums1, nums2, (n1+n2)/2)
+		r := findKth(nums1, nums2, (n1+n2)/2+1)
+		return float64(l+r)/2
+	}
+	return float64(findKth(nums1, nums2, (n1+n2)/2+1))
+}
+
+// 在两个有序数组中查找第 k 大的数
+// 第 k 大的数即 nums 中索引为 k-1 的数，在舍弃区域比较值时都要 -1 处理
+func findKth(nums1, nums2 []int, k int) int {
+	n1, n2 := len(nums1), len(nums2)
+	if n1 > n2 {
+		n1, n2 = n2 , n1
+		nums1, nums2 = nums2, nums1
+	}
+	if n1 == 0{
+		return nums2[k-1]
+	}
+	if k == 1{
+		return min(nums1[0], nums2[0])
+	}
+	k1 := min(k/2, n1)
+	k2 := k - k1
+
+	switch{
+		case nums1[k1-1] < nums2[k2-1]:
+			return findKth(nums1[k1:], nums2, k2)
+		case nums1[k1-1] > nums2[k2-1]:
+			return findKth(nums1, nums2[k2:], k1)
+		default:
+			return nums1[k1-1]
+	}
+}
+
+func min(x, y int) int{
+	if x > y{
+		return y
+	}
+	return x
+}
