a8e8bf339d7cc20513ea7aab0f77022ef28e6afe
libaoan <baoan_008@163.com>
2021-03-05 06:46:33 +0800

    add solution for integer_to_roman

---
+++ string/12_integer_to_roman.go
@@ -0,0 +1,40 @@
+package main
+import "fmt"
+
+func main(){
+	fmt.Println(intToRoman(1994)) //MCMXCIV
+	fmt.Println(intToRoman(20))  // XX
+}
+
+var (
+	m = map[int]string{
+		1: "I",
+		4: "IV",
+		5: "V",
+		9: "IX",
+		10: "X",
+		40: "XL",
+		50: "L",
+		90: "XC",
+		100: "C",
+		400: "CD",
+		500: "D",
+		900: "CM",
+		1000: "M",
+	}
+	units = []int{1000,900,500,400,100,90,50,40,10,9,5,4,1}
+)
+
+func intToRoman(num int) string{
+	var roman string
+	for num > 0{
+		for _, unit := range units{
+			if num/unit > 0{
+				roman += m[unit]
+				num -= unit
+				break
+			}
+		}
+	}
+	return roman
+}
