eca8e56caf1d56c063cbc08306e56527782aa995
libaoan <baoan_008@163.com>
2021-05-11 07:31:20 +0800

    53. 最大子序和 暴力算法和贪心算法

---
+++ array/53_maximum-subarray.go
@@ -0,0 +1,64 @@
+package main
+
+import "fmt"
+
+func main() {
+	nums := []int{-2, 1, -3, 4, -1, 2, 1, -5, 4}
+	//nums = []int{-10000}
+	res := maxSubArray(nums)
+	res = maxSubArray2(nums)
+
+	fmt.Printf("the res:", res)
+}
+
+// 暴力法
+func maxSubArray(nums []int) int {
+	length := len(nums)
+	max := nums[0]
+	for i := 0; i < length; i++ {
+		for j := 1; j <= length-i; j++ {
+			sum := sumSlice(nums[i : i+j])
+			if sum > max {
+				max = sum
+			}
+		}
+	}
+	return max
+}
+
+func sumSlice(nums []int) int {
+	sum := 0
+	for _, n := range nums {
+		sum += n
+	}
+	return sum
+}
+
+// 贪心算法
+func maxSubArray2(nums []int) int {
+	length := len(nums)
+	sum := nums[0]
+	max := nums[0]
+	for i := 1; i < length; i++ {
+		if sum < 0 {
+			sum = nums[i]
+		} else {
+			sum = sum + nums[i]
+		}
+
+		if max < sum {
+			max = sum
+		}
+	}
+	return max
+}
+
+// TODO: 动态规划算法
+func maxSubArray3(nums []int) int {
+	return 0
+}
+
+// TODO: 分治算法
+func maxSubArray4(nums []int) int {
+	return 0
+}
