4bbf4863c9dc6d68cdd69bbc947123d7c67ec25b
libaoan <baoan_008@163.com>
2021-05-15 18:52:18 +0800

    118. 杨辉三角 O(n*n) 代码优化

--- array/118_pascals-triangle.go
+++ array/118_pascals-triangle.go
@@ -4,6 +4,7 @@ import "fmt"
 
 func main() {
 	nums := generate(8)
+	nums = generate2(8)
 	for _, num := range nums {
 		for _, n := range num {
 			fmt.Print(n, ",")
@@ -12,7 +13,7 @@ func main() {
 	}
 }
 
-// 基础算法
+//  基础算法, 代码优化
 func generate(numRows int) [][]int {
 
 	var nums [][]int
@@ -22,24 +23,31 @@ func generate(numRows int) [][]int {
 		} else {
 			newNums := make([]int, i/2+1)
 			newNums[0] = 1
-			if (i+1)%2 == 0 {
-				mid := i / 2
-				for j := 1; j <= mid; j++ {
-					newNums[j] = nums[i-1][j-1] + nums[i-1][j]
-				}
-				for j := mid; j >= 0; j-- {
-					newNums = append(newNums, newNums[j])
-				}
-			} else {
-				mid := i / 2
-				for j := 1; j <= mid; j++ {
-					newNums[j] = nums[i-1][j-1] + nums[i-1][j]
-				}
-				for j := mid - 1; j >= 0; j-- {
-					newNums = append(newNums, newNums[j])
-				}
+			mid := i / 2
+			for j := 1; j <= mid; j++ {
+				newNums[j] = nums[i-1][j-1] + nums[i-1][j]
+			}
+			if i%2 == 0 {
+				mid -= 1
+			}
+			for j := mid; j >= 0; j-- {
+				newNums = append(newNums, newNums[j])
 			}
 			nums = append(nums, newNums)
+		}
+	}
+	return nums
+}
+
+// 别人家的代码， 算法复杂度相同，但是更代码优雅
+func generate2(numRows int) [][]int {
+	nums := make([][]int, numRows)
+	for i := range nums {
+		nums[i] = make([]int, i+1)
+		nums[i][0] = 1
+		nums[i][i] = 1
+		for j := 1; j < i; j++ {
+			nums[i][j] = nums[i-1][j-1] + nums[i-1][j]
 		}
 	}
 	return nums
