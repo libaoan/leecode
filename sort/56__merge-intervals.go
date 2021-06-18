580db906125965ac811470b70424fde4dc027e3a
libaoan <baoan_008@163.com>
2021-06-19 07:46:13 +0800

    56. 合并区间 基础算法，测试通过

--- sort/56__merge-intervals.go
+++ sort/56__merge-intervals.go
@@ -8,13 +8,16 @@ import (
 func main() {
 	intervals := [][]int{[]int{1, 3}, []int{2, 6}, []int{8, 10}, []int{15, 18}}
 	//intervals = [][]int{[]int{1,4}, []int{0,2}, []int{3,5}}
+	intervals = [][]int{
+		[]int{1, 3}, []int{0, 2}, []int{2, 3}, []int{4, 6}, []int{4, 5}, []int{5, 5}, []int{0, 2}, []int{3, 3},
+	}
 	result := merge(intervals)
 	for _, res := range result {
 		fmt.Print(res, " ")
 	}
 }
 
-// 基础算法 todo 调试不完全
+// 基础算法
 func merge(intervals [][]int) [][]int {
 
 	sort.Slice(intervals, func(i, j int) bool {
@@ -26,6 +29,10 @@ func merge(intervals [][]int) [][]int {
 	for j < len(intervals) {
 		if intervals[i][1] < intervals[j][0] {
 			for j < len(intervals) && intervals[j-1][1] >= intervals[j][0] {
+				intervals[j][0] = intervals[j-1][0]
+				if intervals[j-1][1] > intervals[j][1] {
+					intervals[j][1] = intervals[j-1][1]
+				}
 				j++
 			}
 			if intervals[i][1] > intervals[j-1][1] {
@@ -36,6 +43,12 @@ func merge(intervals [][]int) [][]int {
 			i = j
 			j++
 		} else {
+			if intervals[j-1][1] >= intervals[j][0] {
+				intervals[j][0] = intervals[j-1][0]
+			}
+			if intervals[j-1][1] > intervals[j][1] {
+				intervals[j][1] = intervals[j-1][1]
+			}
 			j++
 		}
 	}
