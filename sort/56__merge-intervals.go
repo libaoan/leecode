6ff57611d1862b323754a3d4b9bdd6e99c834c20
libaoan <baoan_008@163.com>
2021-06-19 08:36:12 +0800

    56. 合并区间 优化算法2，测试未通过

--- sort/56__merge-intervals.go
+++ sort/56__merge-intervals.go
@@ -11,7 +11,7 @@ func main() {
 	intervals = [][]int{
 		[]int{1, 3}, []int{0, 2}, []int{2, 3}, []int{4, 6}, []int{4, 5}, []int{5, 5}, []int{0, 2}, []int{3, 3},
 	}
-	result := merge(intervals)
+	result := merge2(intervals)
 	for _, res := range result {
 		fmt.Print(res, " ")
 	}
@@ -62,5 +62,39 @@ func merge(intervals [][]int) [][]int {
 		result = append(result, []int{intervals[i][0], intervals[j-1][1]})
 	}
 
+	return result
+}
+
+// 别人的算法，我没想到，先实现一下
+func merge2(intervals [][]int) [][]int {
+	sort.Slice(intervals, func(i, j int) bool {
+		return intervals[i][0] < intervals[j][0] || (intervals[i][0] == intervals[j][0] && intervals[i][1] < intervals[j][1])
+	})
+
+	result := [][]int{}
+	ln := len(intervals)
+	i, j := 0, 1
+	t := intervals[0][1]
+	max := intervals[ln-1][1]
+	for _, x := range intervals {
+		if max < x[1] {
+			max = x[1]
+		}
+	}
+
+	for t < max {
+		if j < ln && t < intervals[j][0] {
+			result = append(result, []int{intervals[i][0], t})
+			i = j
+			j++
+			t = intervals[i][1]
+		} else if j < ln {
+			t = intervals[j][1]
+			j++
+		} else {
+			result = append(result, []int{intervals[i][0], t})
+		}
+	}
+	result = append(result, []int{intervals[i][0], t})
 	return result
 }
