1e1d465d0f5f473888cfbb34f3fa4cdd6ccc0ae8
libaoan <baoan_008@163.com>
2021-06-29 07:16:13 +0800

    1030. 距离顺序排列矩阵单元格，几何距离法调试通过（时间复杂度N**3）

--- sort/1030_matrix-cells-in-distance-order.go
+++ sort/1030_matrix-cells-in-distance-order.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
 	res := allCellsDistOrder(2, 3, 1, 2)
@@ -15,58 +18,25 @@ func allCellsDistOrder(rows int, cols int, rCenter int, cCenter int) [][]int {
 	res := [][]int{}
 
 	res = append(res, []int{rCenter, cCenter})
-	i, j := 0, 0
+	i := 1
 
 	for {
 		found := false
-
-		if rCenter-i >= 0 {
-			found = true
-			res = append(res, []int{rCenter - i, cCenter})
+		for r := 0; r < rows; r++ {
+			for c := 0; c < cols; c++ {
+				if math.Abs(float64(rCenter-r))+math.Abs(float64(cCenter-c)) == float64(i) {
+					found = true
+					res = append(res, []int{r, c})
+				}
+			}
 		}
-
-		if cCenter-i >= 0 {
-			found = true
-			res = append(res, []int{rCenter, cCenter - i})
-		}
-
-		if rCenter+i < rows {
-			found = true
-			res = append(res, []int{rCenter + i, cCenter})
-		}
-
-		if cCenter+i < cols {
-			found = true
-			res = append(res, []int{rCenter, cCenter + i})
-		}
-
-		if rCenter-i >= 0 && cCenter-i >= 0 {
-			found = true
-			res = append(res, []int{rCenter - i, cCenter - i})
-		}
-
-		if rCenter-i >= 0 && cCenter+i < cols {
-			found = true
-			res = append(res, []int{rCenter - i, cCenter + i})
-		}
-
-		if rCenter+i < rows && cCenter-i >= 0 {
-			found = true
-			res = append(res, []int{rCenter + i, cCenter - i})
-		}
-
-		if rCenter+i < rows && cCenter+i < cols {
-			found = true
-			res = append(res, []int{rCenter + i, cCenter + i})
-		}
-
 		if found == false {
 			return res
 		} else {
 			i++
 		}
-
 	}
+
 }
 
 // todo 桶排序
