6404e331bb3d83f1d440b1565273a9efe55c1651
libaoan <baoan_008@163.com>
2021-08-12 07:46:22 +0800

    110. 平衡二叉树 迭代算法，从数据构造二叉树失败

--- tree/110_balanced-binary-tree.go
+++ tree/110_balanced-binary-tree.go
@@ -1,13 +1,61 @@
 package main
 
+import (
+	"fmt"
+	"strconv"
+)
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func main() {
+	nums := []string{"null", "1", "2", "2", "3", "3", "null", "null", "4", "4"}
+	nums = []string{"null", "2", "1", "4", "null", "null", "3", "6", "null", "null", "5"}
+
+	// todo: 构造失败
+	root := CreateTree(nums)
+
+	fmt.Println(isBalanced(root))
+}
+
+func CreateTree(nums []string) *TreeNode {
+	if len(nums) < 1 {
+		return nil
+	}
+	return Create(nums, 1)
+}
+
+func Create(nums []string, index int) *TreeNode {
+
+	if nums[index] == "null" {
+		return nil
+	}
+
+	num, _ := strconv.Atoi(nums[index])
+	root := &TreeNode{num, nil, nil}
+	leftI, rightI := 2*index, 2*index+1
+	if leftI < len(nums) && nums[leftI] != "null" {
+		root.Left = Create(nums, leftI)
+	}
+
+	if rightI < len(nums) && nums[rightI] != "null" {
+		root.Right = Create(nums, rightI)
+	}
+
+	return root
+}
+
 func isBalanced(root *TreeNode) bool {
 
 	if root == nil {
 		return true
 	}
 
-	leftD := getDeep(root.Left)
-	rightD := getDeep(root.Right)
+	leftD := getDeep2(root.Left)
+	rightD := getDeep2(root.Right)
 
 	if leftD-rightD > 1 || rightD-leftD > 1 {
 		return false
@@ -32,6 +80,26 @@ func getDeep(root *TreeNode) int {
 }
 
 // todo 迭代算法
-func getDeep2(root *TreeNode) int {
 
+func getDeep2(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+
+	left, right := 0, 0
+
+	for root.Left != nil {
+		root = root.Left
+		left++
+	}
+
+	for root.Right != nil {
+		root = root.Right
+		right++
+	}
+	if left >= right {
+		return left + 1
+	} else {
+		return right + 1
+	}
 }
