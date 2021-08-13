ae42ca2859693f117e1e34607634534d884bb118
libaoan <baoan_008@163.com>
2021-08-13 10:57:35 +0800

    110. 平衡二叉树 迭代算法

--- tree/110_balanced-binary-tree.go
+++ tree/110_balanced-binary-tree.go
@@ -13,38 +13,42 @@ type TreeNode struct {
 
 func main() {
 	nums := []string{"null", "1", "2", "2", "3", "3", "null", "null", "4", "4"}
-	nums = []string{"null", "2", "1", "4", "null", "null", "3", "6", "null", "null", "5"}
+	nums = []string{"2", "1", "4", "null", "null", "3", "6", "null", "null", "5"}
+	nums = []string{"1", "2"}
 
-	// todo: 构造失败
 	root := CreateTree(nums)
 
 	fmt.Println(isBalanced(root))
 }
 
+// 构造二叉树
 func CreateTree(nums []string) *TreeNode {
-	if len(nums) < 1 {
+	if len(nums) == 0 || nums[0] == "null" {
 		return nil
 	}
-	return Create(nums, 1)
-}
-
-func Create(nums []string, index int) *TreeNode {
-
-	if nums[index] == "null" {
-		return nil
-	}
-
-	num, _ := strconv.Atoi(nums[index])
+	queue := []*TreeNode{}
+	num, _ := strconv.Atoi(nums[0])
 	root := &TreeNode{num, nil, nil}
-	leftI, rightI := 2*index, 2*index+1
-	if leftI < len(nums) && nums[leftI] != "null" {
-		root.Left = Create(nums, leftI)
-	}
+	queue = append(queue, root)
+	for index := 0; index < len(nums) && len(queue) > 0; index += 2 {
+		node := queue[0]
+		if len(queue) > 0 {
+			queue = queue[1:]
+		}
 
-	if rightI < len(nums) && nums[rightI] != "null" {
-		root.Right = Create(nums, rightI)
+		if index+1 < len(nums) && nums[index+1] != "null" {
+			num, _ := strconv.Atoi(nums[index+1])
+			newn := &TreeNode{num, nil, nil}
+			node.Left = newn
+			queue = append(queue, newn)
+		}
+		if index+2 < len(nums) && nums[index+2] != "null" {
+			num, _ := strconv.Atoi(nums[index+2])
+			newn := &TreeNode{num, nil, nil}
+			node.Right = newn
+			queue = append(queue, newn)
+		}
 	}
-
 	return root
 }
 
@@ -54,8 +58,8 @@ func isBalanced(root *TreeNode) bool {
 		return true
 	}
 
-	leftD := getDeep2(root.Left)
-	rightD := getDeep2(root.Right)
+	leftD := getDeep2(root.Left) + 1
+	rightD := getDeep2(root.Right) + 1
 
 	if leftD-rightD > 1 || rightD-leftD > 1 {
 		return false
@@ -79,27 +83,31 @@ func getDeep(root *TreeNode) int {
 	}
 }
 
-// todo 迭代算法
-
+// 迭代算法
 func getDeep2(root *TreeNode) int {
 	if root == nil {
 		return 0
 	}
+	queue := []*TreeNode{root}
+	depth := 0
 
-	left, right := 0, 0
+	for len(queue) > 0 {
 
-	for root.Left != nil {
-		root = root.Left
-		left++
+		subL := len(queue)
+		for subL > 0 {
+			node := queue[0]
+			if len(queue) > 0 {
+				queue = queue[1:]
+			}
+			if node.Left != nil {
+				queue = append(queue, node.Left)
+			}
+			if node.Right != nil {
+				queue = append(queue, node.Right)
+			}
+			subL--
+		}
+		depth++
 	}
-
-	for root.Right != nil {
-		root = root.Right
-		right++
-	}
-	if left >= right {
-		return left + 1
-	} else {
-		return right + 1
-	}
+	return depth
 }
