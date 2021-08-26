ee4c71121764a9925fec40f39277664d3da9901e
libaoan <baoan_008@163.com>
2021-08-27 07:28:20 +0800

    145. 二叉树的后序遍历 Morris 遍历(参考)

--- tree/145_binary-tree-postorder-traversal.go
+++ tree/145_binary-tree-postorder-traversal.go
@@ -67,3 +67,39 @@ func postorderTraversal2(root *TreeNode) (res []int) {
 	}
 	return
 }
+
+func reverse(a []int) {
+	for i, n := 0, len(a); i < n/2; i++ {
+		a[i], a[n-1-i] = a[n-1-i], a[i]
+	}
+}
+
+// todo Morris 遍历
+func postorderTraversal3(root *TreeNode) (res []int) {
+	addPath := func(node *TreeNode) {
+		resSize := len(res)
+		for ; node != nil; node = node.Right {
+			res = append(res, node.Val)
+		}
+		reverse(res[resSize:])
+	}
+
+	p1 := root
+	for p1 != nil {
+		if p2 := p1.Left; p2 != nil {
+			for p2.Right != nil && p2.Right != p1 {
+				p2 = p2.Right
+			}
+			if p2.Right == nil {
+				p2.Right = p1
+				p1 = p1.Left
+				continue
+			}
+			p2.Right = nil
+			addPath(p1.Left)
+		}
+		p1 = p1.Right
+	}
+	addPath(root)
+	return
+}
