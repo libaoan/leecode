0783afae9c074d72d73f010beb1633ed27975a2b
libaoan <baoan_008@163.com>
2021-09-14 07:18:20 +0800

    501. 二叉搜索树中的众数 中序遍历+递归

--- tree/501_find-mode-in-binary-search-tree.go
+++ tree/501_find-mode-in-binary-search-tree.go
@@ -32,4 +32,36 @@ func visitAndCount(root *TreeNode, maps map[int]int) {
 	visitAndCount(root.Left, maps)
 }
 
-// 进阶算法
+// 进阶算法 todo:参考
+
+func findMode1(root *TreeNode) []int {
+	res := []int{}
+	var base, count, maxCount int
+
+	update := func(x int) {
+		if x == base {
+			count++
+		} else {
+			base, count = x, 1
+		}
+		if count == maxCount {
+			res = append(res, base)
+		} else if count > maxCount {
+			maxCount = count
+			res = []int{base}
+		}
+	}
+
+	var dfs func(*TreeNode)
+	dfs = func(node *TreeNode) {
+		if node == nil {
+			return
+		}
+		dfs(node.Left)
+		update(node.Val)
+		dfs(node.Right)
+	}
+	dfs(root)
+
+	return res
+}
