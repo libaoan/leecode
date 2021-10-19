cd42b253c8c1f6ca4e0966332d2a8d51ffa9ddb2
libaoan <baoan_008@163.com>
2021-10-20 07:31:40 +0800

    617. 合并二叉树,  广度迭代算法未实现

--- tree/617_merge-two-binary-trees.go
+++ tree/617_merge-two-binary-trees.go
@@ -24,3 +24,50 @@ func mergeTrees(root1 *TreeNode, root2 *TreeNode) *TreeNode {
 
 	return root1
 }
+
+// todo 迭代法
+func mergeTrees2(root1 *TreeNode, root2 *TreeNode) *TreeNode {
+
+	p1, p2 := root1, root2
+	p := p1
+	if p1 == nil {
+		p = p2
+	}
+	leftq, rightq := []*TreeNode{}, []*TreeNode{}
+	if p1.Left != nil {
+		leftq = append(leftq, p1.Left)
+	}
+
+	if p2.Left != nil {
+		leftq = append(leftq, p2.Left)
+	}
+
+	if p1.Right != nil {
+		rightq = append(rightq, p1.Right)
+	}
+
+	if p2.Right != nil {
+		rightq = append(rightq, p2.Right)
+	}
+
+	for len(leftq) != 0 || len(rightq) != 0 {
+
+		if p1 != nil && p2 != nil {
+			p.Val = p1.Val + p2.Val
+
+		}
+
+	}
+
+	for root1 != nil || root2 != nil {
+		if root1 != nil && root2 != nil {
+			root1.Val += root2.Val
+			p1l = root1.Left
+			p1r = root1.Right
+
+			p2l = root2.Left
+			p2r = root2.Right
+		}
+	}
+
+}
