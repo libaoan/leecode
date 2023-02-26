a1ee8c0b8e69ca1b7dcf96215d5b1de6b7177b11
libaoan <baoan_008@163.com>
2023-02-27 07:37:06 +0800

    lengthOflastWord 优化2 todo：内存挑战不到50%

--- string/58_lengthOfLastWord.go
+++ string/58_lengthOfLastWord.go
@@ -11,7 +11,7 @@ func lengthOfLastWord(s string) int {
 	s = strings.TrimSpace(s)
 	for _, c := range s {
 		if c == ' ' {
-			cnt = 1
+			cnt = 0
 		} else {
 			cnt += 1
 		}
@@ -20,8 +20,27 @@ func lengthOfLastWord(s string) int {
 	return cnt
 }
 
+func lengthOfLastWord2(s string) int {
+
+	cnt := 0
+	index := len(s) - 1
+
+	for s[index] == ' ' {
+		index--
+	}
+
+	for index >= 0 && s[index] != ' ' {
+		cnt++
+		index--
+		//fmt.Printf("%v, %v, %c\n", index, cnt, s[index])
+	}
+
+	return cnt
+}
+
 func main() {
 	var s string
-	s = "Hello World"
+	s = "Hello World  "
 	fmt.Println(lengthOfLastWord(s))
+	fmt.Println(lengthOfLastWord2(s))
 }
