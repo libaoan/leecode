0022909790c7c55d96ddfc3c0f1017a511f2d8d1
libaoan <baoan_008@163.com>
2023-03-01 07:22:37 +0800

    409. 最长回文串 todo 速度39%， 内存5%， 待优化

---
+++ string/409_longestPalindrome.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func longestPalindrome(s string) int {
+
+	cnt := 0
+	maps := make(map[int32]int)
+	for _, c := range s {
+		_, ok := maps[c]
+		if ok {
+			maps[c]++
+		} else {
+			maps[c] = 1
+		}
+	}
+
+	type Kvs struct {
+		Key   int32
+		Value int
+	}
+
+	lists := make([]Kvs, 0)
+	for k, v := range maps {
+		if maps[k]%2 == 0 {
+			cnt += maps[k]
+		} else if maps[k] > 1 {
+			cnt = cnt + maps[k] - 1
+		}
+		lists = append(lists, Kvs{Key: k, Value: v})
+	}
+
+	sort.Slice(lists, func(i, j int) bool {
+		return lists[i].Value > lists[j].Value
+	})
+
+	fmt.Println(lists)
+
+	for _, kvs := range lists {
+		if kvs.Value%2 == 1 {
+			return cnt + 1
+		}
+	}
+
+	return cnt
+}
+
+func main() {
+
+	s := "aaaaaccc"
+	fmt.Println(longestPalindrome(s))
+}
