47ad5bfb08c8f043d97b7259035926ab0914c1bb
libaoan <baoan_008@163.com>
2023-03-02 07:22:16 +0800

    455. 分发饼干 速度93%， 内存99%， 时间复杂度(nlogn + mlogm)

--- array/455_findContentChildren.go
+++ array/455_findContentChildren.go
@@ -5,7 +5,7 @@ import (
 	"sort"
 )
 
-// 贪心， n2
+// 贪心， nm
 func findContentChildren(g []int, s []int) int {
 	var cnt int
 
@@ -30,8 +30,25 @@ func findContentChildren(g []int, s []int) int {
 	return cnt
 }
 
+// 贪心， n2
+func findContentChildren2(g []int, s []int) int {
+	var cnt int
+
+	sort.Ints(s)
+	sort.Ints(g)
+
+	for i, j := 0, 0; i < len(g) && j < len(s); j++ {
+		if g[i] <= s[j] {
+			i++
+			cnt++
+		}
+	}
+
+	return cnt
+}
+
 func main() {
 	g := []int{1, 1}
 	s := []int{1, 1, 1}
-	fmt.Println(findContentChildren(g, s))
+	fmt.Println(findContentChildren2(g, s))
 }
