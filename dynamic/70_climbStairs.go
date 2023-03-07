8e8b2fb4aec60a62b64a705f35e8bbe6ed3bff72
libaoan <baoan_008@163.com>
2023-03-08 07:04:29 +0800

    70. 爬楼梯 动态规划，算法速度100%， 内存50%， 时间复杂度(n)

---
+++ dynamic/70_climbStairs.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+// 动态规划  算法复杂度n， 空间复杂度 n
+// 推倒公式 f(n) = f(n-1) + f(n-1)
+func climbStairs(n int) int {
+
+	if n <= 0 {
+		return 0
+	}
+	if n == 1 {
+		return 1
+	}
+
+	cnts := make([]int, n+1)
+	cnts[1] = 1
+	cnts[2] = 2
+
+	for i := 3; i <= n; i++ {
+		cnts[i] = cnts[i-1] + cnts[i-2]
+	}
+	return cnts[n]
+}
+
+func main() {
+	fmt.Println(climbStairs(6))
+}
