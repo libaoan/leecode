7cfb194126e7a913e72f829a25f1b5dfe6eebb90
libaoan <baoan_008@163.com>
2023-03-13 00:31:17 +0800

    # 322. 零钱兑换 递归回朔，结果正确，运行超时

--- dynamic/322_coinChange.go
+++ dynamic/322_coinChange.go
@@ -2,80 +2,51 @@ package main
 
 import (
 	"fmt"
-	"sort"
+	// "sort"
 	"time"
 )
 
-// todo: 未实现 不能在单层循环内实现 贪心算法
-func coinChange2(coins []int, amount int) int {
-
-	n := len(coins)
-	if n == 0 && amount == 0 {
-		return 0
-	} else if n == 0 {
-		return -1
-	} else {
-		sort.Ints(coins)
-		cnt, amountT := 0, amount
-
-		for i := n - 1; i >= 0 && amountT > 0; i-- {
-			d := amountT / coins[i]
-			amountT = amountT % coins[i]
-			cnt += d
-		}
-
-		if amountT > 0 {
-			cnt = -1
-		}
-
-		res := coinChange(coins[:n-1], amount)
-		fmt.Println(coins, res, cnt)
-		if res > 0 && res < cnt {
-			return res
-		} else if res == 0 {
-			return 0
-		} else {
-			return cnt
-		}
-
-	}
-
-}
-
+// 递归回朔算法 结果正确，但是运行超时 用例通过率30%
 func coinChange(coins []int, amount int) int {
 
+	return doCoinChange(coins, amount, 0)
+}
+
+func doCoinChange(coins []int, amount int, cnt int) int {
 	n := len(coins) - 1
 	if amount == 0 {
-		return 0
+		return cnt
 	}
-	if amount < 0 || n < 1 {
+	if amount < 0 || n < 0 {
 		return -1
 	}
 
-	cnt := 0
-	cnt1 := coinChange(coins, amount-coins[n])
-	cnt0 := coinChange(coins[:n], amount)
-	// fmt.Println(coins, cnt1, coins[:n], cnt0)
-	if cnt1 < 0 && cnt0 < 0 {
+	cnt0, cnt1 := -1, -1
+	if amount-coins[n] >= 0 {
+		cnt1 = doCoinChange(coins, amount-coins[n], cnt+1)
+	}
+	if n > 0 {
+		cnt0 = doCoinChange(coins[:n], amount, cnt)
+	}
+
+	if cnt0 < 0 && cnt1 < 0 {
 		return -1
 	}
+
 	if cnt1 < 0 {
 		return cnt0
 	}
+
 	if cnt0 < 0 {
-		return cnt1 + 1
-	}
-	if cnt1 == 0 && cnt0 == 0 {
-		return 1
-	}
-	if cnt0 == 0 || cnt1 == 0 {
-		return cnt1 + 1
+		return cnt1
 	}
 
-	if cnt0 > cnt1+1 {
+	if cnt0 < cnt1 {
 		cnt = cnt0
+		fmt.Println(coins, coins[:n], amount, cnt)
 	} else {
-		cnt = cnt1 + 1
+		cnt = cnt1
+		fmt.Println(coins, coins[:n], amount, cnt)
 	}
 
 	return cnt
@@ -85,10 +56,9 @@ func main() {
 	start := time.Now().UnixNano()
 	coins := []int{1, 2, 5}
 	amount := 11
-	coins = []int{186, 419, 83, 408}
-	amount = 6249
-	sort.Ints(coins)
-	fmt.Println(coins)
+	coins = []int{411, 412, 413, 414, 415, 416, 417, 418, 419, 420, 421, 422}
+	amount = 9864
+	//sort.Ints(coins)
 	fmt.Println(coinChange(coins, amount))
 	fmt.Println("运行时间", time.Now().UnixNano()-start)
 }
