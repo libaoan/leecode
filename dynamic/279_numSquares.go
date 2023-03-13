262bbd00ee76ebacbb6d7de78f2c1b53b4aba238
libaoan <baoan_008@163.com>
2023-03-13 22:37:27 +0800

    # 279. 完全平方数 动态规划实现 速度43%， 内存92%  有优化空间， 复杂度n2

--- dynamic/279_numSquares.go
+++ dynamic/279_numSquares.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"time"
 )
 
@@ -20,7 +21,7 @@ func numSquares2(n int) int {
 		if minus == 0 {
 			return 1
 		} else if minus > 0 {
-			cnts = append(cnts, numSquares(minus)+1)
+			cnts = append(cnts, numSquares2(minus)+1)
 		}
 
 	}
@@ -35,8 +36,32 @@ func numSquares2(n int) int {
 	return ans
 }
 
+// 动态规划实现 速度43%， 内存92%  有优化空间， 复杂度n2
+func numSquares(n int) int {
+
+	min := func(i, j int) int {
+		if i <= j {
+			return i
+		}
+		return j
+	}
+
+	cnts := make([]int, n+1)
+	for i := 0; i <= n; i++ {
+		cnts[i] = math.MaxInt
+	}
+	cnts[0] = 0
+	cnts[1] = 1
+	for i := 2; i <= n; i++ {
+		for j := 1; i >= j*j; j++ {
+			cnts[i] = min(cnts[i-j*j]+1, cnts[i])
+		}
+	}
+	return cnts[n]
+}
+
 func main() {
 	start := time.Now().UnixNano()
-	fmt.Println(numSquares2(58))
+	fmt.Println(numSquares(12))
 	fmt.Println("运行时间", time.Now().UnixNano()-start)
 }
