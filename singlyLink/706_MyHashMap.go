aad7e0c15c6fd047854c99f850ecea46c249fc6c
libaoan <baoan_008@163.com>
2023-03-25 07:48:54 +0800

    706. 设计哈希映射 // todo: 通过率 20%，待继续定位

--- singlyLink/706_MyHashMap.go
+++ singlyLink/706_MyHashMap.go
@@ -1,12 +1,15 @@
 package main
 
+import "fmt"
+
 type Data struct {
 	value int
+	key   int
 	next  *Data
 }
 
 type MyHashMap struct {
-	data []Data
+	data []*Data
 	cap  int
 }
 
@@ -15,49 +18,80 @@ func Constructor() MyHashMap {
 }
 
 func (this *MyHashMap) hash(key int) int {
+	if this.cap == 0 {
+		return key
+	}
 	return key % this.cap
 }
 
 func (this *MyHashMap) Put(key int, value int) {
+
 	if this.cap == 0 || this.cap <= this.hash(key) {
-		cap_ := this.cap + key
+		cap_ := this.cap + this.hash(key)
 		tmp := MyHashMap{
-			data: make([]Data, cap_),
+			data: make([]*Data, cap_),
 			cap:  cap_,
 		}
-		for k, v := range this.data {
-			tmp.data[k] = v
+		for _, v := range this.data {
+			for v != nil {
+				tmp.Put(v.key, v.value)
+				v = v.next
+			}
 		}
-		this = &tmp
+		*this = tmp
 	}
 
-	key = this.hash(key)
-	da := Data{}
-	if this.data[key] == da {
-		da.value = value
-		this.data[key] = da
-	} else {
-		cur := this.data[key]
-		for cur.value != value && cur.next != nil {
-			cur = *(cur.next)
-		}
-		if cur.value == value {
-			return
-		}
-		da.value = value
-		cur.next = &da
+	k := this.hash(key)
+	pre, cur := this.data[k], this.data[k]
+	for cur != nil && cur.key != key {
+		pre = cur
+		cur = cur.next
 	}
+	if pre == nil {
+		da := &Data{key: key, value: value}
+		this.data[k] = da
+		return
+	}
+	if cur != nil && cur.key == key {
+		cur.value = value
+		return
+	}
+	da := &Data{key: key, value: value}
+	da.value = value
+	da.key = key
+	pre.next = da
 }
 
 func (this *MyHashMap) Get(key int) int {
-	key = this.hash(key)
-	if this.cap <= key {
+	k := this.hash(key)
+	if this.cap <= k {
 		return -1
 	}
+	cur := this.data[k]
+	for cur != nil && cur.key != key {
+		cur = cur.next
+	}
+	if cur == nil {
+		return -1
+	}
+	return cur.value
 }
 
 func (this *MyHashMap) Remove(key int) {
-
+	k := this.hash(key)
+	if this.cap <= k {
+		return
+	}
+	pre, cur := this.data[k], this.data[k]
+	for cur != nil && cur.key != key {
+		pre = cur
+		cur = cur.next
+	}
+	if cur == nil {
+		return
+	}
+	pre.next = cur.next
+	cur.next = nil
 }
 
 /**
@@ -67,3 +101,15 @@ func (this *MyHashMap) Remove(key int) {
  * param_2 := obj.Get(key);
  * obj.Remove(key);
  */
+
+func main() {
+	obj := Constructor()
+	obj.Put(1, 1)
+	obj.Put(2, 2)
+	fmt.Println(obj.Get(1))
+	fmt.Println(obj.Get(3))
+	fmt.Println(obj.Get(2))
+	obj.Put(2, 1)
+	//fmt.Println(obj.data[0].next)
+	fmt.Println(obj.Get(2))
+}
