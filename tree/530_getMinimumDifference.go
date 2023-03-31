c2dab824122e71ba04ceed71c49985b868ca54ef
libaoan <baoan_008@163.com>
2023-03-31 23:22:24 +0800

    530 二叉搜索树的最小绝对差 // todo: 深度遍历， 通过率50%

--- tree/530_getMinimumDifference.go
+++ tree/530_getMinimumDifference.go
@@ -68,3 +68,128 @@ func getMinimumDifference(root *TreeNode) int {
 
 	return ans
 }
+
+// todo: 深度遍历， 通过率50%
+func getMinimumDifference2(root *TreeNode) int {
+
+	if root == nil {
+		return 0
+	}
+	queue := []*TreeNode{}
+	mins := math.MaxInt
+	node := root
+	// 左树
+	for node != nil {
+		for node.Left != nil {
+			if mins > (node.Val - node.Left.Val) {
+				mins = node.Val - node.Left.Val
+			}
+			node = node.Left
+			queue = append(queue, node)
+		}
+
+		if len(queue) == 0 {
+			break
+		}
+
+		if node.Right != nil {
+			if mins > node.Right.Val-node.Val {
+				mins = node.Right.Val - node.Val
+			}
+			if mins > root.Val-node.Val {
+				mins = root.Val - node.Val
+			}
+			if len(queue) < 1 {
+				break
+			}
+			queue = queue[:len(queue)-1]
+			node = node.Right
+			queue = append(queue, node)
+		} else {
+			if mins > root.Val-node.Val {
+				mins = root.Val - node.Val
+			}
+			if len(queue) < 2 {
+				break
+			}
+			queue = queue[:len(queue)-1]
+			node = queue[len(queue)-1]
+			if mins > root.Val-node.Val {
+				mins = root.Val - node.Val
+			}
+			if node.Right != nil {
+				if mins > node.Right.Val-node.Val {
+					mins = node.Right.Val - node.Val
+				}
+				if mins > root.Val-node.Val {
+					mins = root.Val - node.Val
+				}
+				if len(queue) < 2 {
+					break
+				}
+				queue = queue[:len(queue)-1]
+				queue = append(queue, node.Right)
+			}
+			node = node.Right
+		}
+	}
+
+	// 右树
+	node = root.Right
+	queue = []*TreeNode{node}
+	if node == nil {
+		return mins
+	}
+	if mins > node.Val-root.Val {
+		mins = node.Val - root.Val
+	}
+	for node != nil {
+		for node.Left != nil {
+			if mins > node.Val-node.Left.Val {
+				mins = node.Val - node.Left.Val
+			}
+			node = node.Left
+			queue = append(queue, node)
+		}
+
+		if len(queue) == 0 {
+			break
+		}
+
+		if node.Right != nil {
+			if mins > node.Right.Val-node.Val {
+				mins = node.Right.Val - node.Val
+			}
+			if mins > node.Val-root.Val {
+				mins = node.Val - root.Val
+			}
+			queue = queue[:len(queue)-1]
+			node = node.Right
+			queue = append(queue, node)
+		} else {
+			if mins > node.Val-root.Val {
+				mins = node.Val - root.Val
+			}
+			if len(queue) < 2 {
+				break
+			}
+			queue = queue[:len(queue)-1]
+			node = queue[len(queue)-1]
+			if node.Right != nil {
+				if mins > node.Right.Val-node.Val {
+					mins = node.Right.Val - node.Val
+				}
+				if mins > node.Val-root.Val {
+					mins = node.Val - root.Val
+				}
+				if len(queue) < 2 {
+					break
+				}
+				queue = queue[:len(queue)-1]
+				queue = append(queue, node.Right)
+			}
+			node = node.Right
+		}
+	}
+	return mins
+}
