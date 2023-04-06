82c5e358b23c44b295a19e3ffa23154f7b3d5d06
libaoan <baoan_008@163.com>
2023-04-07 00:05:49 +0800

    10. 正则表达式匹配 todo : 通过率90%, 待完善

--- string/10_isMatch.go
+++ string/10_isMatch.go
@@ -5,7 +5,7 @@ import "fmt"
 // 1. p 不是点,且后面不是*， 直接匹配，不相同返回false，相同下一位
 // 2. p 是点，且后面是星，直接匹配到p的倒数第星位
 // 3. p 是点，s下一位
-// todo : 通过率80%, 待完善
+// todo : 通过率90%, 待完善
 func isMatch(s string, p string) bool {
 	sLen, pLen := len(s), len(p)
 
@@ -27,12 +27,8 @@ func isMatch(s string, p string) bool {
 		if i == pLen {
 			// i== 1， 说明后面没有*
 			if i == 1 {
-				if s[0] == p[0] {
-					if len(s) > 1 {
-						return false
-					} else {
-						return true
-					}
+				if s[0] == p[0] && len(s) == 1 {
+					return true
 				} else {
 					return false
 				}
@@ -44,8 +40,9 @@ func isMatch(s string, p string) bool {
 				}
 			}
 			return true
+
 		} else {
-			// i== 1， 说明后面没有*
+			// i == 1， 说明后面没有*
 			if i == 1 {
 				if s[0] == p[0] {
 					if len(s) > 1 {
@@ -58,16 +55,16 @@ func isMatch(s string, p string) bool {
 				}
 			}
 
+			// 如果后面有* todo
 			j := 0
-			for ; j < sLen; j++ {
-				if s[j] != p[0] {
-					return false
-				}
+			for ; j < sLen && s[j] == p[0]; j++ {
+				// 如果s[j] 和 p[0] 相等，继续往后匹配
 			}
-			if j == sLen {
+			// p = .* 模式
+			if i >= sLen {
 				return false
 			}
-			return isMatch(s[j:], p[i:])
+			return isMatch(s[pLen-i+1:], p[i:])
 		}
 
 	} else {
@@ -77,14 +74,27 @@ func isMatch(s string, p string) bool {
 		for i < pLen && p[i] == '*' {
 			i++
 		}
-		// p已经结尾, 说明为 .** 模式，完全匹配
+		// p 已经匹配结束
 		if i == pLen {
+			// i == 1， .为最后一位，如果s还有多余位数，不匹配
+			if i == 1 && len(s) > 1 {
+				return false
+			}
+			// p已经结尾, 说明为 .* 模式，完全匹配
 			return true
 		} else {
+			// p[0] == ., 则匹配一位，继续往后
+			if i == 1 && len(s) > 1 {
+				return isMatch(s[1:], p[i:])
+			}
+			if i == 1 {
+				return true
+			}
+			// p = .* 模式
 			if i >= sLen {
 				return false
 			}
-			return isMatch(s[i:], p[i:])
+			return isMatch(s[sLen-i+1:], p[i:])
 		}
 	}
 
@@ -95,5 +105,6 @@ func main() {
 	s, p = "aa", "a*"
 	s, p = "ab", ".*"
 	s, p = "aab", "c*a*b"
+	s, p = "aaa", "a*a"
 	fmt.Println(isMatch(s, p))
 }
