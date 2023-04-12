4a20bc5f075dbf5ad097a3dddd6fe4544d9de45f
libaoan <baoan_008@163.com>
2023-04-13 07:07:09 +0800

    136. 只出现一次的数字 hashmap 速度 2N 25%, 内存 N 18%

--- array/136_singleNumber.go
+++ array/136_singleNumber.go
@@ -14,6 +14,26 @@ func singleNumber(nums []int) int {
 	return nums[i]
 }
 
+// hashmap 速度 2N 25%, 内存 N 18%
+func singleNumber2(nums []int) int {
+	n := len(nums)
+	maps := make(map[int]bool, n/2+1)
+	for _, i := range nums {
+		if _, ok := maps[i]; !ok {
+			maps[i] = false
+		} else {
+			maps[i] = true
+		}
+	}
+
+	for k, v := range maps {
+		if !v {
+			return k
+		}
+	}
+	return -1
+}
+
 func main() {
 
 }
