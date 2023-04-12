e169ad15e0a93c39d3a60a9ae3c3214dfa716a36
libaoan <baoan_008@163.com>
2023-04-13 07:19:46 +0800

    136. 只出现一次的数字
    // hashmap优化 速度 N 66%, 内存 N 18%
    // 数学算法， 速度 N 96%， 内存 1 98%

--- array/136_singleNumber.go
+++ array/136_singleNumber.go
@@ -34,6 +34,38 @@ func singleNumber2(nums []int) int {
 	return -1
 }
 
+// hashmap优化 速度 N 66%, 内存 N 18%
+func singleNumber3(nums []int) int {
+	n := len(nums)
+	maps := make(map[int]struct{}, n/2+1)
+	for _, i := range nums {
+		if _, ok := maps[i]; !ok {
+			maps[i] = struct{}{}
+		} else {
+			delete(maps, i)
+		}
+	}
+
+	for k, _ := range maps {
+		return k
+	}
+	return -1
+}
+
+// 参考，别人的算法，通过位运算实现 a ^ 0 = 0 and a ^ a = 0
+// 看来位运算还是一个常考的点：
+// 1、 二进制一样的位为0， 不一样的位为1
+// 2、相同的数字异或为0，0与任何数字异或结果是它本身
+// 3、异或运算的法则类似乘法：满足交换律、结合律。a ^ b = b ^ a  (a ^ b) ^ c = a ^ (b ^ c)
+// 数学算法， 速度 N 96%， 内存 1 98%
+func singleNumber4(nums []int) int {
+	single := 0
+	for _, num := range nums {
+		single ^= num
+	}
+	return single
+}
+
 func main() {
 
 }
