649fa666849a7e8bbea612acf5fca0b63773f5c5
libaoan <baoan_008@163.com>
2023-05-11 08:03:03 +0800

    45. 跳跃游戏 II
    // todo: 通过率20%

---
+++ array/45.jump.go
@@ -0,0 +1,44 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	nums := []int{2, 3, 1, 1, 4}
+	nums = []int{1, 1, 1, 1}
+	nums = []int{1, 2, 1, 1, 1}
+	nums = []int{4, 1, 1, 3, 1, 1, 1}
+	fmt.Println(jump(nums))
+
+}
+
+// todo: 通过率20%
+func jump(nums []int) int {
+
+	max := func(i, j int, nums []int) int {
+		for k := i + 1; k <= j; k++ {
+			if nums[k] > nums[i] {
+				i = k
+			}
+		}
+		return i
+	}
+
+	i, jmp, n := 0, 1, len(nums)-1
+
+	if n < 1 {
+		return 0
+	}
+
+	for i+nums[i] < n {
+		jmp++
+		step := max(i, i+nums[i], nums)
+		if step == i {
+			i += nums[i]
+		} else {
+			i = step
+		}
+		println("-", i)
+	}
+	return jmp
+}
