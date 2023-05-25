d2889b9d4e332d5abcfb69068759772b7e426770
libaoan <baoan_008@163.com>
2023-05-26 07:50:07 +0800

    1013. 将数组分成和相等的三个部分 提示 todo: 别人的思路, 双指针 O(n) 速度 96%， 内存 43%

--- array/1013.canThreePartsEqualSum.go
+++ array/1013.canThreePartsEqualSum.go
@@ -1,5 +1,13 @@
 package main
 
+import "fmt"
+
+func main() {
+	arr := []int{3, 3, 6, 5, -2, 2, 5, 1, -9, 4}
+	arr = []int{1, -1, 1, -1}
+	fmt.Println(canThreePartsEqualSum3(arr))
+}
+
 // O(n3) 速度 96%， 内存 64%
 func canThreePartsEqualSum(arr []int) bool {
 	sum, midIndexs, n := 0, []int{}, len(arr)
@@ -64,6 +72,39 @@ func canThreePartsEqualSum2(arr []int) bool {
 		if cur == 2*target {
 			return true
 		}
+	}
+	return false
+
+}
+
+// todo: 别人的思路, 双指针 O(n) 速度 96%， 内存 43%
+func canThreePartsEqualSum3(arr []int) bool {
+	n, target := len(arr), 0
+	for i := 0; i < n; i++ {
+		target += arr[i]
+	}
+	if target%3 != 0 {
+		return false
+	}
+
+	target /= 3
+	isum, jsum, i, j := arr[0], arr[n-1], 1, n-2
+
+	for i < j {
+		if isum != target {
+			isum += arr[i]
+			i++
+		}
+
+		if jsum != target {
+			jsum += arr[j]
+			j--
+		}
+
+		if i <= j && isum == target && jsum == target {
+			return true
+		}
+
 	}
 	return false
 
