480ce9b6e959979c8ef051c9c2e727f6dfc9652c
libaoan <baoan_008@163.com>
2023-05-30 07:29:33 +0800

    # 704. 二分查找 todo 参考答案 速度 46%， 内存 98%

--- array/704.search.go
+++ array/704.search.go
@@ -7,7 +7,7 @@ func main() {
 	nums, target := []int{-1, 0, 3, 5, 9, 12}, 9
 	nums, target = []int{-1, 0, 3, 5, 9, 12}, 2
 	nums, target = []int{2, 5}, 5
-	fmt.Println(search(nums, target))
+	fmt.Println(search2(nums, target))
 }
 
 // O(logn) 速度 97%, 内存 60%
@@ -34,6 +34,23 @@ func search(nums []int, target int) int {
 			hi = mid
 		}
 		//fmt.Println(lo, hi, mid)
+	}
+	return -1
+}
+
+// todo 参考答案 速度 46%， 内存 98%
+func search2(nums []int, target int) int {
+	left, right := 0, len(nums)-1
+	for left <= right {
+		mid := (right-left)/2 + left
+		num := nums[mid]
+		if num == target {
+			return mid
+		} else if num > target {
+			right = mid - 1
+		} else {
+			left = mid + 1
+		}
 	}
 	return -1
 }
