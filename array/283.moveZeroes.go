544a2f6ff2bc4e978935871629dc5b3daeac0642
libaoan <baoan_008@163.com>
2023-05-31 07:21:01 +0800

    # 283. 移动零 // O(n) 速度25% 内存5%

--- array/283.moveZeroes.go
+++ array/283.moveZeroes.go
@@ -1,6 +1,6 @@
 package main
 
-// O(n) 速度95% 内存5%
+// O(2n) 速度95% 内存5%
 func moveZeroes(nums []int) {
 	left, n := 0, len(nums)
 
@@ -16,4 +16,18 @@ func moveZeroes(nums []int) {
 		nums[left] = 0
 		left++
 	}
+}
+
+// O(n) 速度25% 内存5%
+func moveZeroes2(nums []int) {
+	left, n := 0, len(nums)
+
+	for i := 0; i < n; i++ {
+		if nums[i] != 0 {
+			nums[left], nums[i] = nums[i], nums[left]
+			left++
+		}
+
+	}
+
 }
