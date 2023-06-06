25ceed1a66bf34cbbeecb80942f9e872ac2fad25
libaoan <baoan_008@163.com>
2023-06-07 07:19:02 +0800

    2460. 对数组执行操作 参考 O(n), 一次遍历

--- array/2460.applyOperations.go
+++ array/2460.applyOperations.go
@@ -1,6 +1,6 @@
 package main
 
-// O(n)， 速度 60%， 内存100%
+// O(3n)， 速度 60%， 内存100%
 func applyOperations(nums []int) []int {
 	cnt0, n := 0, len(nums)
 	for i := 0; i < n-1; i++ {
@@ -25,6 +25,23 @@ func applyOperations(nums []int) []int {
 	for j < n {
 		nums[j] = 0
 		j++
+	}
+	return nums
+}
+
+// 参考 O(n), 一次遍历
+func applyOperations2(nums []int) []int {
+	n := len(nums)
+	j := 0
+	for i := 0; i < n; i++ {
+		if i+1 < n && nums[i] == nums[i+1] {
+			nums[i] *= 2
+			nums[i+1] = 0
+		}
+		if nums[i] != 0 {
+			nums[i], nums[j] = nums[j], nums[i]
+			j++
+		}
 	}
 	return nums
 }
