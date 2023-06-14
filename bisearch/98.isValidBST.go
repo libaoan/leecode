04288df9bdb9d76c5784f462d0939fa6d44535f6
libaoan <baoan_008@163.com>
2023-06-15 07:24:35 +0800

    # 98. 验证二叉搜索树 // dfs T=41% S=50%

--- bisearch/98.isValidBST.go
+++ bisearch/98.isValidBST.go
@@ -4,24 +4,55 @@ func main() {
 
 }
 
-//Definition for a binary tree node.
+// Definition for a binary tree node.
 type TreeNode struct {
 	Val   int
 	Left  *TreeNode
 	Right *TreeNode
 }
 
-// dfs todo 通过率 90%
+// dfs T=41% S=50%
 func isValidBST(root *TreeNode) bool {
+
 	if root == nil {
 		return true
 	}
-	if root.Left != nil && root.Left.Val >= root.Val {
-		return false
+	return check(root, root.Left) && check(root, root.Right)
+
+}
+
+func check(root, cur *TreeNode) bool {
+
+	if cur == nil {
+		return true
 	}
-	if root.Right != nil && root.Right.Val <= root.Val {
+
+	if cur.Left != nil && cur.Left.Val >= cur.Val {
 		return false
 	}
 
-	return isValidBST(root.Left) && isValidBST(root.Right)
+	if cur.Right != nil && cur.Right.Val <= cur.Val {
+		return false
+	}
+
+	return check(root, cur.Left) && check(root, cur.Right) && bst(root, cur)
+}
+
+func bst(root, cur *TreeNode) bool {
+
+	if root == nil {
+		return false
+	}
+
+	if root.Val < cur.Val {
+		return bst(root.Right, cur)
+	} else if root.Val > cur.Val {
+		return bst(root.Left, cur)
+	} else {
+		if root != cur {
+			return false
+		} else {
+			return true
+		}
+	}
 }
