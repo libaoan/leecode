c0a5dacf7a1557325d92802f1b4333e87c75c4cd
libaoan <baoan_008@163.com>
2023-06-20 07:53:01 +0800

    # 1091. 二进制矩阵中的最短路径 // 全遍历 todo  通过率 56%

--- array/1091.shortestPathBinaryMatrix.go
+++ array/1091.shortestPathBinaryMatrix.go
@@ -17,6 +17,19 @@ func main() {
 		{1, 1, 0},
 		{1, 1, 1},
 	}
+	grid = [][]int{
+		{0, 0, 0},
+		{1, 0, 0},
+		{1, 1, 0},
+	}
+	grid = [][]int{
+		{0, 1, 0, 0, 0, 0},
+		{0, 1, 0, 1, 1, 0},
+		{0, 1, 1, 0, 1, 0},
+		{0, 0, 0, 0, 1, 0},
+		{1, 1, 1, 1, 1, 0},
+		{1, 1, 1, 1, 1, 0},
+	}
 	fmt.Println(shortestPathBinaryMatrix(grid))
 	for _, gs := range grid {
 		fmt.Println(gs)
@@ -31,22 +44,28 @@ func shortestPathBinaryMatrix(grid [][]int) int {
 	return dfs(grid, 0, 0, 0, n)
 }
 
-// 全遍历 todo  通过率 25%
+// 全遍历 todo  通过率 56%
 func dfs(grid [][]int, i, j, step, n int) int {
 
 	if i == n && j == n {
 		return step + 1
 	}
-	if i > n || j > n || i < 0 || j < 0 {
-		return -1
-	}
-	grid[i][j] = 1
+
+	grid[i][j] = 2
 
 	ds := [8]int{-1, -1, -1, -1, -1, -1, -1, -1}
 
 	if i < n && j < n && grid[i+1][j+1] == 0 {
 		ds[0] = dfs(grid, i+1, j+1, step, n)
 	}
+
+	if i < n && grid[i+1][j] == 0 {
+		ds[5] = dfs(grid, i+1, j, step, n)
+	}
+	if j < n && grid[i][j+1] == 0 {
+		ds[7] = dfs(grid, i, j+1, step, n)
+	}
+
 	if i > 0 && j < n && grid[i-1][j+1] == 0 {
 		ds[1] = dfs(grid, i-1, j+1, step, n)
 	}
@@ -59,21 +78,24 @@ func dfs(grid [][]int, i, j, step, n int) int {
 	if i > 0 && grid[i-1][j] == 0 {
 		ds[4] = dfs(grid, i-1, j, step, n)
 	}
-	if i < n && grid[i+1][j] == 0 {
-		ds[5] = dfs(grid, i+1, j, step, n)
-	}
 	if j > 0 && grid[i][j-1] == 0 {
 		ds[6] = dfs(grid, i, j-1, step, n)
 	}
-	if j < n && grid[i][j+1] == 0 {
-		ds[7] = dfs(grid, i, j+1, step, n)
-	}
 
 	best := -1
+	fmt.Println(ds, i, j)
 	for _, s := range ds {
-		if s != -1 && best < s {
+		if best < 0 {
+			best = s
+		} else if best < s {
 			best = s
 		}
 	}
-	return best + 1
+
+	if best < 0 {
+		grid[i][j] = 0
+	} else {
+		best++
+	}
+	return best
 }
