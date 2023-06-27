027fe2f5615c288be26df6c4dfc45345e1abc9ab
libaoan <baoan_008@163.com>
2023-06-28 07:17:01 +0800

    849. 到最近的人的最大距离 // O(n)  T=46%, S=73%

--- array/849.maxDistToClosest.go
+++ array/849.maxDistToClosest.go
@@ -6,10 +6,14 @@ func main() {
 	seats := []int{
 		1, 0, 0, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0,
 	}
+	seats = []int{
+		0, 0, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0,
+	}
+	seats = []int{0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 1}
 	fmt.Println(maxDistToClosest(seats))
 }
 
-// todo P=90%
+// O(n)  T=46%, S=73%
 func maxDistToClosest(seats []int) int {
 	ans, pos, n := -1, -1, len(seats)
 	for i := 0; i < n; i++ {
@@ -28,8 +32,15 @@ func maxDistToClosest(seats []int) int {
 			pos = i
 		}
 	}
+	fmt.Println(seats)
 	if ans == 1 || pos == ans-1 || pos == n-1 {
 		return ans
+	}
+	if (ans+1)/2 < seats[n-1] {
+		return seats[n-1]
+	}
+	if (ans+1)/2 < seats[(ans+1)/2] && seats[(ans+1)/2] == (ans+1)/2+1 {
+		return seats[(ans+1)/2]
 	}
 	return (ans + 1) / 2
 }
